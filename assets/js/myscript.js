9432a32578dce2d0b95b0de0f699368602bee9d0
FABIO CURCI <fabiocurci@Air-di-FABIO.homenet.telecomitalia.it>
2024-06-12 17:45:53 +0200

    added menu

---
+++ assets/js/myscript.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Data target da confrontare (formato YYYY-MM-DD)
+    const targetDate = '2024-06-12';
+
+    // Ottieni la data corrente
+    const currentDate = new Date();
+    const currentDateString = currentDate.toISOString().split('T')[0]; // formato YYYY-MM-DD
+
+    // Confronta le date
+    if (currentDateString === targetDate) {
+        // Se coincidono, nascondi il div
+        console.log("date coincidono");
+        //document.getElementById('root').style.display = 'none';
+        window.location.href = "menu/index.html";
+    }
+});
+
+
