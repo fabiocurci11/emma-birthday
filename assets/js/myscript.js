cc7bd5fc8bd55be65082b25f16060a036fe8172b
FABIO CURCI <fabiocurci@Air-di-FABIO.homenet.telecomitalia.it>
2024-06-12 17:57:23 +0200

    restart date

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     // Data target da confrontare (formato YYYY-MM-DD)
-    const targetDate = '2024-06-12';
+    const targetDate = '2024-06-16';
 
     // Ottieni la data corrente
     const currentDate = new Date();
